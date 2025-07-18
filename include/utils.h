f2f97bb714c9734d67c3f4abc721ec8558e39abf
AYn0nyme <aynoblox@gmail.com>
2025-07-17 23:31:38 -0400

    feat: cp command

--- include/utils.h
+++ include/utils.h
@@ -7,5 +7,6 @@
 int parse_args(int argc, char** argv, struct option options[], size_t options_count);
 
 #define array_len(array) (sizeof(array) / sizeof(*array))
+#define opt(n,c,f,d) { .name = n,.small = c,.flag = f,.description = d }
 
 #endif
---
+++ src/cp.c
@@ -0,0 +1,54 @@
+#include "../include/utils.h"
+#include <stdio.h>
+
+#define FLAG_THING (1 << 2)
+
+const char* usage = "cp [SOURCE] [DESTINATION]\ncopy the content of SOURCE to DESTINATION";
+
+struct option options[] = {
+  opt("force",'f',FLAG_THING,"force create file if exists"),
+};
+
+void cp(const char* from, const char* to, int flags) {
+  FILE* file = fopen(from, "r");
+  if (file == NULL) {
+    printf("cp : file %s not found", from);
+    return;
+  }
+  FILE* new_file = fopen(to, "r");
+  if(new_file != NULL && !(flags & FLAG_THING)) {
+    printf("cp : file %s already exists", to);
+    return;
+  }
+  new_file = fopen(to, "w");
+  char c;
+  while((c = fgetc(file)) != EOF) {
+    fputc(c, new_file);
+  }
+
+  fclose(file);
+  fclose(new_file);
+}
+
+int main(int argc, char** argv) {
+  int flags = parse_args(argc, argv, options, array_len(options));
+
+  char* from;
+  char* to;
+  
+  int count = 0;
+  for(int c = 1; c < argc; c++) {
+    if(argv[c][0] == '-') continue;
+    if(count == 0) {
+      from = argv[c];
+    } else if (count == 1) {
+      to = argv[c];
+    }
+    count++;
+  }
+  if (count == 0) {
+    puts("cp : missing arguments");
+    return 1;
+  }
+  cp(from, to, flags);
+}
